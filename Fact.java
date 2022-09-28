292a2e1a22ed2ac463656b785e6e1b89d3cd99f2
Mekhadev2025 <98373907+Mekhadev2025@users.noreply.github.com>
2022-09-28 15:39:57 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,14 @@
+import java.io.*;
+ 
+public class Fact
+{
+public static void main(String args[])
+{ 
+int num=3;int m=num;
+ int fact=1;
+  for(int i=1;i<=3;i++)
+  fact=fact*i;
+ System.out.println("Factorial "+fact);
+
+}
+}
