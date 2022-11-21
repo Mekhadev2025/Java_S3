9368674c7f2ff9116c1ec00db48d74a2aeede277
Mekha L <mekhalnair@gmail.com>
2022-11-21 12:21:49 +0530

    Update BinarySearch.java

--- Cycle5/BinarySearch.java
+++ Cycle5/BinarySearch.java
@@ -1,4 +1,4 @@
-import java.util.*;
+ import java.util.*;
 class BinarySearch{  
     public  void binarySearch(int arr[], int first, int last, int key){  
       int mid = (first + last)/2;  
@@ -19,6 +19,46 @@ class BinarySearch{
          System.out.println("Element is not found!");  
       }  
     }  
+
+
+    public void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public  int partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low - 1);
+  
+        for (int j = low; j <= high - 1; j++) {
+   
+            if (arr[j] < pivot) {
+                 i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+    public   void quickSort(int[] arr, int low, int high)
+    {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    } 
+    void printArray(int[] arr, int size)
+    {
+        for (int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+  
+        System.out.println();
+    }
+  
+
     public static void main(String args[])
     {  
            int a[]=new int[10];
@@ -34,6 +74,8 @@ class BinarySearch{
            int item=sc.nextInt();
             
           BinarySearch B =new BinarySearch();
+           B.quickSort(a, 0, n-1);
+           B.printArray(a, n);
           B.binarySearch(a,0, n, item);
            
                }  
